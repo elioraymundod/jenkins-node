883c52560600284745ec2a5051b80ecb00333ec6
Elio Raymundo <raymundoelio@gmail.com>
2021-10-02 14:21:50 -0600

    se modifica pantalla de inicio

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+          <h1 className="App-title">Curso de Sistemas Operativos</h1>
         </header>
         <p className="App-intro">
-          Welcome to this Docker Meetup with Jenkins, Curso de SO I
+          Universidad Mariano Galvez, Curso de SO, Elio Raymundo
 
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
