223ea6bb4c2b33647f86226fce18e1dace6676b1
Elio Raymundo <raymundoelio@gmail.com>
2021-10-27 22:15:21 -0600

    Se agrega pantalla inicial con mision, vision y objetivos

--- src/App.js
+++ src/App.js
@@ -8,13 +8,60 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Curso de Sistemas Operativos</h1>
+          <h1 className="App-title">Sistema Administrador de Ventas</h1>
         </header>
-        <p className="App-intro">
-          Universidad Mariano Galvez, Curso de SO, Elio, Raul, Debora, Gabriela
 
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-white">
+          <h1 className="App-vision">Visión</h1>
+          <p className="App-intro">
+            Somos una empresa líder en productos de tecnología e innovación, 
+          </p>
+          <p className="App-intro">
+            buscamos ayudar a las personas a adquirir los últimos productos en tecnología.
+          </p>
+          <p className="App-intro">
+            Somos un excelente lugar para trabajar, en donde nuestro personal puede desarrollar sus habilidades y actitudes
+          </p>
+          <p className="App-intro">
+          dando lo mejor de sí para crecer juntos.
+          </p>
+        </div>
+
+        <hr></hr>
+        <div className="App-white "><br></br><br></br><br></br>
+        <h1 className="App-vision">Misión</h1>
+          <p className="App-intro">
+            Brindar a través de nuestros productos la mejor satisfacción a todos nuestros clientes.
+          </p>
+          <p className="App-intro">
+          Brindar los mejores productos del mercado a los mejores precios del mercado a nuestra clientela.
+          </p>
+        </div>
+
+        <hr></hr>
+        <div className="App-white "><br></br><br></br><br></br>
+        <h1 className="App-vision">Objetivos</h1>
+          <p className="App-intro">
+          •	Ser la empresa lider en tecnología de Guatemala.
+          </p>
+          <p className="App-intro">
+          •	Tener varias sucursales en los diferentes departamentos de Guatemala.
+          </p>
+          <p className="App-intro">
+          •	Proveer empleo a los guatemaltecos más necesitados.
+          </p>
+          <p className="App-intro">
+          •	Contribuir al desarrollo tecnológico de Guatemala.
+          </p>
+          <p className="App-intro">
+          •	Crear productos innovadores en el mundo tecnológico.
+          </p>
+        </div><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+
+        <div className="App-footer">
+          <p>Universidad Mariano Galvez de Guatemala, Todos los derechos reservados, 2021</p>
+        </div>
+        
       </div>
     );
   }
